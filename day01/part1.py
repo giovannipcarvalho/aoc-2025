b63446031652adfb27ef575eb230dd32798209b1
Giovanni Paolo <giovanni1995@gmail.com>
2025-12-01 20:22:42 +0100

    day01/part1

---
+++ day01/part1.py
@@ -0,0 +1,40 @@
+import sys
+
+input = """\
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""
+expected = 3
+
+
+def main(input: str) -> int:
+    lock = 50
+    # number of times landed on position 0
+    result = 0
+    for instruction in input.splitlines():
+        direction = 1 if instruction[0] == "R" else -1
+        amount = int(instruction[1:])
+
+        lock += direction * amount
+        lock = lock % 100
+
+        if lock == 0:
+            result += 1
+
+    return result
+
+
+def test_part1() -> None:
+    assert main(input) == expected
+
+
+if __name__ == "__main__":
+    print(main(sys.stdin.read()))
